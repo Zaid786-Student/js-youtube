5f3523a8be3e3b9f975e05a9527e88ebd3af4368
Zaid786-Student <khanzaid63945@gmail.com>
2025-07-05 10:56:25 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,36 @@
+let score = false
+
+console.log(typeof score);
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber );
+
+console.log(valueInNumber );
+
+/* when we converted no. like 33 so in terminal we get number , numer , number
+when we converter no. 33 into string using "" this then we get string string no. 
+when we wrote 33abc then NaN(not a number) aa raha hai
+similarly with null object came and with undefined ,, undefined come 
+with boolean we get boolean and if true the +1 otherwise 0 
+we can conver string into no. and no. into string ex let valueInNumber = Number(score) string to no. 
+and no. to string simply us e"""
+*/
+
+let isLoggedIn = "null"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) //no. to boolean
+
+console.log(booleanIsLoggedIn);
+
+// greater than 1 is true in boolean
+//string is true but something should be inside 
+//0 is false in boolean
+//null is false and empty string is also false ""this
+
+let num = 25;
+
+let stringNumber = String(num);
+console.log(typeof stringNumber);
+//numer to string
