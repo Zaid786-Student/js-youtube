7fa3b66cdd0241ec108990d55f91111057ba1023
Zaid786-Student <khanzaid63945@gmail.com>
2025-07-05 05:50:54 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"Use strict"; //treat all JS code as newer version
+
+//alert (3+3) //in node we get error in alert but not in browser
+
+console.log(3+3)
+
+console.log("Zaid")
+
+let name = "Zaid"
+let age = 18
+let isLoggedIn = false
+let accountCity;
+let statent = null
+
+/* number => 2 to power 
+bigint
+ string => ""
+ boolean => true/false
+ null => variable hai but variable is emply in value
+undefined => no value in variable
+symbol => use for uniqueness
+
+
+
+object */
+
+console.log(typeof null);
+console.log(typeof zaid);
+console.log(typeof null);
+console.log(typeof undefined);
