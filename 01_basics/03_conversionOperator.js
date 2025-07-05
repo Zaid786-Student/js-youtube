49225e21aed51c6bb272eb11a6350d7780b7d8a4
Zaid786-Student <khanzaid63945@gmail.com>
2025-07-05 11:29:48 +0000

    new lesson

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -1,13 +1,13 @@
 let score = false
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof (score));
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber );
+// console.log(typeof valueInNumber );
 
-console.log(valueInNumber );
+// console.log(valueInNumber );
 
 /* when we converted no. like 33 so in terminal we get number , numer , number
 when we converter no. 33 into string using "" this then we get string string no. 
@@ -22,7 +22,7 @@ let isLoggedIn = "null"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn) //no. to boolean
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // greater than 1 is true in boolean
 //string is true but something should be inside 
@@ -32,5 +32,46 @@ console.log(booleanIsLoggedIn);
 let num = 25;
 
 let stringNumber = String(num);
-console.log(typeof stringNumber);
-//numer to string
+// console.log(typeof stringNumber);
+//numer to string 
+
+
+
+//********************Operations*********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2); //additon
+// console.log(2-2); // subtraction
+// console.log(2*2); //multiplication
+// console.log(2**2); //power me lana//
+// console.log(2/3);  //divide
+// console.log(2%3);  //remainder nikalna
+
+
+let str1 = "hello"
+let str2 = " Zaid"
+ //how to add two variable in same line
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// //basically adding string and number infact aligning side by side 
+// console.log("1" + 2 +3);
+// //agar string pahle hai to sab as a string side by side  add hote hai 
+// console.log("1" + 2 + 3 + 4);
+// //leekinn agar string last me ya bich me matlb jaha tak string nahi aa raha basically string ke baad
+// //sare element side by side add honge par string se pahle no. addition follow karenge
+// console.log(2 + 5 + 8 + 9 +"1" +2 +3);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2 //very  bad thing in cod it look messi
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
