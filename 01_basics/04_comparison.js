a0246582514ba8c694623c19c96dbd6eedbb97f0
Zaid786-Student <khanzaid63945@gmail.com>
2025-07-05 11:47:38 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log( 2 > 1);
+// console.log( 2 < 1);
+// console.log( 2 >= 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0); 
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+//these are so confusion so avoid it 
+// ===strictly check with datatype also
+
+console.log("2" === 2); //false because it check datatype also string and no. 
+console.log("2" == 2); // true becouse first string converted number then compare not check datatype only element
+
