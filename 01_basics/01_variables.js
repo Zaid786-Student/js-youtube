6be0fb2550f183f87ddcbe8678a26ee79832ddd2
Zaid786-Student <khanzaid63945@gmail.com>
2025-07-05 05:39:50 +0000

    new lesson

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "khanzaid@google.com"
+var accountPassward = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+accountEmail = "za@mail.com"
+accountPassward = "21211"
+accountCity = "lucknow"
+
+console.log(accountId)
+
+/*prefer not to use var because of issue in block scope */
+
+console.table ([accountId, accountEmail, accountPassward, accountCity]);
