63f4f9a954ed1cb15970e6af331bda0752b9ca41
Zaid786-Student <khanzaid63945@gmail.com>
2025-07-10 02:52:26 +0000

    new file

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,13 @@
+console.log("Z");
+console.log("i");
+console.log("i");
+console.log("d");
+
+functioncsayMyName() {
+    console.log("Z");
+    console.log("i");
+    console.log("i");
+    console.log("d");
+
+}
+sayMyName()
