b10f2cd35daea6fc49c43359ac7de90a4213e4b1
Zaid786-Student <khanzaid63945@gmail.com>
2025-07-10 11:11:46 +0000

    hosting basics

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,8 +4,47 @@ let a = 300
 if (true) {                       //if ke ander ke scope{} ko kahte hai block scope
     let a = 10;
     const c= 30;
-    console.log("INNER", a);
+    // console.log("INNER", a);
     
 }
-console.log(a);
+// console.log(a);
 // console.log(c);
+
+function one() {
+    const username = "Zaid"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+    
+}           //ander ka function bahar ke function ko access karte hai
+
+// one()
+
+if (true) {
+    const username ="Zaid"
+     if (username === "Zaid") {
+        const website = "youtube"
+        // console.log(username + website);
+        
+     }
+
+    //  console.log(username);
+}
+
+//++++++++++++intresting concept++++++++++++++++++++++++++++++++++//
+
+function addone(num) {
+    return num +1
+}
+
+console.log(addone(5));
+
+const addTwo = function(num) {
+    return num + 2
+}
+console.log(addTwo(6));
