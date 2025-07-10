9ae4e9439b2f81bb7ffdcbd4307257fb06804fa1
Zaid786-Student <khanzaid63945@gmail.com>
2025-07-10 03:16:51 +0000

    wiil continue

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,13 +1,26 @@
-console.log("Z");
-console.log("i");
-console.log("i");
-console.log("d");
+//functions start
 
-functioncsayMyName() {
+function sayMyName() {
     console.log("Z");
     console.log("i");
     console.log("i");
     console.log("d");
 
 }
-sayMyName()
+// sayMyName() //it will run when you use scopes() otherwise it will know but not run
+
+// function addTwoNumber(num1, num2) {         //jub number introduce karate hai to wo parameter kahlate hai
+//     console.log(num1 + num2);
+    
+// }
+function addTwoNumber(num1, num2) {         
+    // let result = num1 + num2
+    // return result
+    return num1 + num2 //better way
+    
+}
+const result = addTwoNumber(3, 4)  //jab no. ko call karate hai to use argument kahte hai
+
+// console.log("Result",result);
+
+
